a556c9d2c3d618a3f4b51f045980908b0954711a
ChangMyeong <fanatic5500@gmail.com>
2022-04-30 17:57:08 +0900

    워크스페이스 페이지 생성, 워크스페이스 내 글 작성 기능 추가해야함, 다른 사람 글에서 워크스페이스로 가져오는 기능 구현 필요

--- workspace/views.py
+++ workspace/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from novel.models import Node
 from .forms import WorkForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-
+@login_required(login_url='account:login_view')
 def index(request):
-    work_list=Node.objects._mptt_filter(author=request.user.nickname,in_work_space=True).order_by('-create_date')
+    work_list=Node.objects._mptt_filter(author=request.user.id,in_work_space=True).order_by('-create_date')
     context={'work_list':work_list}
     return render(request,'workspace/work_list.html',context)
 
